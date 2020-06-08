b3fb3b0eb29e9a07dd82e0b2927cc6b1eabbb316
Ste <cowley05@hotmail.co.uk>
2020-06-08 14:47:03 +0100

    Ste/Gergo - Initial commit

---
+++ src/main/java/com/tw/motorsalesco/MotorSalesCoApplication.java
@@ -0,0 +1,13 @@
+package com.tw.motorsalesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorSalesCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorSalesCoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/motorsalesco/MotorSalesCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.motorsalesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorSalesCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
